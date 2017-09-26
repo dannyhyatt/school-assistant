2d26c147673f6d19b86f49951507b9733a2ec417
Dan <daf281@aol.com>
2017-09-25 21:10:06 -0400

    Add files via upload

---
+++ src/SchoolAssistantBot/Main.java
@@ -0,0 +1,26 @@
+package schoolassistantbot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main {
+	public static void main(String[] args) {
+
+    	System.out.println("started");
+    	
+        // Initialize Api Context
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new SchoolAssistantBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+        
+	}
+}
--- src/SchoolAssistantBot/SchoolAssistantBot.java
+++ src/SchoolAssistantBot/SchoolAssistantBot.java
@@ -1,1 +1,25 @@
+package schoolassistantbot;
 
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class SchoolAssistantBot extends TelegramLongPollingBot {
+
+	@Override
+	public void onUpdateReceived(Update update) {
+		if(update.hasMessage() && update.getMessage().hasText()) {
+			
+		}
+	}
+	
+	@Override
+	public String getBotUsername() {
+		return "SchoolAssistantTestBot";
+	}
+
+	@Override
+	public String getBotToken() {
+		return "API_KEY_HERE";
+	}
+
+}
